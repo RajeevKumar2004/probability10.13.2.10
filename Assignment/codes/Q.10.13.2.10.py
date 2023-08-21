4211794fdafb33e77a179a92b156638a869c5ab6
RajeevKumar2004 <141238628+RajeevKumar2004@users.noreply.github.com>
2023-08-21 21:54:05 +0530

    Add files via upload

--- Assignment/codes/Q.10.13.2.10.py
+++ Assignment/codes/Q.10.13.2.10.py
@@ -5,21 +5,17 @@ from scipy.stats import bernoulli
 #100 samples
 simlen=int(1000)
 i=0
-def simulation(i):
-    #Probability of the event
-    prob = 0.5
-    desired_output=1/8
-    #Generating sample date using Bernoulli r.v.
-    data_bern = bernoulli.rvs(size=simlen,p=prob)
-    #Calculating the number of favourable outcomes
-    err_ind = np.nonzero(data_bern == 1)
-    #calculating the probability of no head on one toss
-    err_n = np.size(err_ind)/simlen
-    #calculating the probability of no head on three toss = prob no head on one toss x prob no head on one toss x prob no head on one toss
-    prob_0_head=round(err_n*err_n*err_n,3)
-    if prob_0_head==desired_output:
-        print('Probability of getting no head in three toss = '+str(prob_0_head))
-        exit()
-    else:
-        simulation(i+1)
-simulation(i)
+#Probability of the event
+prob = 0.5
+desired_output=1/8
+#Generating sample date using Bernoulli r.v.
+data_bern = bernoulli.rvs(size=simlen,p=prob)
+#Calculating the number of favourable outcomes
+err_ind = np.nonzero(data_bern == 1)
+#calculating the probability of no head on one toss 
+err_n = round(np.size(err_ind)/simlen,1)
+#calculating the probability of no head on three toss = prob no head on one toss x prob no head on one toss x prob no head on one toss
+prob_0_head=err_n*err_n*err_n
+if prob_0_head==desired_output:
+    print('Probability of getting no head in three toss = '+str(prob_0_head))
+      
