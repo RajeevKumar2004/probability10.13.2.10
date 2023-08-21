71882548c6acb435c8a665cb00c072eab8760525
RajeevKumar2004 <141238628+RajeevKumar2004@users.noreply.github.com>
2023-08-21 14:55:10 +0530

    Add files via upload

--- Assignment/codes/Q.10.13.2.10.py
+++ Assignment/codes/Q.10.13.2.10.py
@@ -2,55 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 
-#desired outputs
-desired_output=[0.125, 0.375,0.375,0.125]
-head_0_times=0
-head_1_times=0
-head_2_times=0
-head_3_times=0
-#function to simulate result multiple times till we get desired result
-def simulate():
-    #Let number of samples is 3000.
-    simlen=int(3000)
-
-    #Probability of the event 
-    prob = 1/2
-    # let heads=1 and tails=0 
-    head_0_times=0
-    head_1_times=0
-    head_2_times=0
-    head_3_times=0
-    #Generating sample data
+#100 samples
+simlen=int(1000)
+i=0
+def simulation(i):
+    #Probability of the event
+    prob = 0.5
+    desired_output=1/8
+    #Generating sample date using Bernoulli r.v.
     data_bern = bernoulli.rvs(size=simlen,p=prob)
-   
-    # Converting list to group data in threes and count no. heads
-    for i in range (0,2998,3):
-        add=0
-        add=data_bern[i]+data_bern[i+1]+data_bern[i+2]
-        if add == 0:
-            head_0_times+=1
-        elif add==1:
-            head_1_times+=1
-        elif add==2:
-            head_2_times+=1   
-        elif add==3:
-            head_3_times+=1
-    
-    return head_0_times,head_1_times,head_2_times,head_3_times, data_bern  
-#Simulating data by using infinite loop till we get desired value  
-while True:
-    head_0_times,head_1_times,head_2_times,head_3_times, data_bern=simulate()
-    if head_0_times/1000==desired_output[0] and head_1_times/1000==desired_output[1] and head_2_times/1000==desired_output[2]:
-        break
-print("Samples generated:",data_bern)  
-print(f'Probability of head_0_times = {round(head_0_times/1000,3)}')
-print(f'Probability of head_1_times = {round(head_1_times/1000,3)}')
-print(f'Probability of head_2_times = {round(head_2_times/1000,3)}')
-print(f'Probability of head_3_times = {round(head_3_times/1000,3)}')
-         
-
-# Equating given conclusion(head_0_times = 1/4) and rounding of our result for better evaluation
-if round(head_0_times,2)==1/4:
-    print("Given statement that no head = 1/4 is correct")
-else:
-    print ("Given statement that no head = 1/4 is incorrect")
+    #Calculating the number of favourable outcomes
+    err_ind = np.nonzero(data_bern == 1)
+    #calculating the probability of no head on one toss
+    err_n = np.size(err_ind)/simlen
+    #calculating the probability of no head on three toss = prob no head on one toss x prob no head on one toss x prob no head on one toss
+    prob_0_head=round(err_n*err_n*err_n,3)
+    if prob_0_head==desired_output:
+        print('Probability of getting no head in three toss = '+str(prob_0_head))
+        exit()
+    else:
+        simulation(i+1)
+simulation(i)
